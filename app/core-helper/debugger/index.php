769c2d0e091dbeb6c1bb190fe59804bbaaa34338
Pogodin Konstantin <kosmom@kosmom.ru>
2020-08-10 21:15:12 +0300

    core-debugger 0.3 lister

--- app/core-helper/debugger/index.php
+++ app/core-helper/debugger/index.php
@@ -1,6 +1,6 @@
 <?php
 // Name: Code debugger panel
-// Version: 0.2
+// Version: 0.3
 
 
 c\mvc::$noBody=false;
@@ -12,9 +12,15 @@ $path='logs/debugger';
 $log_files=c\filedata::filelist($path,'',function($filename){
 	return basename($filename);
 });
-$current_file=$_GET['file'];
-if ($_GET['file'] && file_exists($path.'/'.$current_file)){
-	for ($a=0;$a<100;$a++){
-		$contents[]= c\input::jsonDecode(c\filedata::readLastDataPart($path.'/'.$current_file));
-	}
+$current_file = $_GET['file'];
+$full_name = $path . '/' . $current_file;
+
+$offset = null;
+if (isset($_GET['offset']))$offset = $_GET['offset'];
+if ($_GET['file'] && file_exists($full_name)){
+	$handler = c\filedata::getHandler($full_name, $offset);
+	for ($a = 0; $a < 100; $a++){
+		$contents[] = c\input::jsonDecode(c\filedata::readLastDataPart($full_name));
+		if (!ftell($handler))break;
+    }
 }
