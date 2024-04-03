837023ed54f4dfdf754d90aead2bbdf65e97a030
Pogodin Konstantin <kosmom@kosmom.ru>
2024-04-03 17:45:13 +0300

    make migrate prettify

--- app/core-helper/migrate/make/index.php
+++ app/core-helper/migrate/make/index.php
@@ -8,8 +8,12 @@ if (!$title){
 	$title=c\mvc::routeNext();
 }
 
-if (!$title)die('not set migration name');
+if (!$title)die(c\cli::COLOR_RED.'not set migration name'.c\cli::COLOR_RESET.PHP_EOL);
 //$table='validate';
 
-file_put_contents('migrations/'.date('Y-m-d-H-i-s').'-'.$title.'.php', file_get_contents(__DIR__.'/template.phtml'));
-die('migration successfully created');
+$result=file_put_contents('migrations/'.date('Y-m-d-H-i-s').'-'.$title.'.php', file_get_contents(__DIR__.'/template.phtml'));
+if ($result){
+    die('migration successfully created'.PHP_EOL);
+}else{
+    die(c\cli::COLOR_RED.'migration create error'.c\cli::COLOR_RESET.PHP_EOL);
+}
